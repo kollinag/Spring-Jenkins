b491147220e21cd22f12da35dbb90e6aea12cf04
Nagarjuna <kollinagarjunareddy123@gmail.com>
2023-05-16 13:18:46 +0530

    third commit

---
+++ src/test/java/com/nag/SpringJenkinsTest.java
@@ -0,0 +1,21 @@
+package com.nag;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringJenkinsTest {
+	
+	public static Logger logger=LoggerFactory.getLogger(SpringJenkinsTest.class);
+	
+	@Test
+	public void hai() {
+		logger.info("hai");
+		assertEquals(true,true);
+	}
+
+}
